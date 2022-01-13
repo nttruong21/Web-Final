0baf36faf1c4922bce2197959b25027f91908911
phamtung-23 <ptung230801@gmail.com>
2022-01-13 15:41:18 +0700

    update folder manager11

--- manager/donDaTao.php
+++ manager/donDaTao.php
@@ -40,25 +40,14 @@
 
 	<div>
 		<div>
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 				<a class="navbar-brand" href="#">Task Manager</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
 				<div class="collapse navbar-collapse " id="navbarSupportedContent">
-					<form class="form-inline my-2 my-lg-0">
-						<input class="box-search mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-					</form>
-						<ul class="navbar-nav mr-auto">
-							<li class="nav-item">
-								<a class="nav-link" href="#"><i class="fas fa-cogs"></i></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#"><i class="fas fa-question-circle"></i></a>
-							</li>
-						</ul>
+						<ul class="navbar-nav mr-auto"></ul>
 					<?php
 						?>
 						<div>
@@ -149,8 +138,7 @@
 				</div>
 				<div  class=" p-2 d-flex justify-content-center">
 					
-            <a href="formNghiPhep.php"><button type="button" class="btn btn-outline-dark m-2 "><i class="fas fa-calendar-plus"></i>  Tạo đơn</button></a>
-						<a href="donDaTao.php"><button type="button" class="btn btn-outline-dark m-2"><i class="far fa-calendar-check"></i>  Đơn đã tạo</button></a>
+            
         </div>
 		</div>
 		</div>
