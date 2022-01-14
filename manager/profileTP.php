e05d5f63f9593fa2b5430c355a0d1985ee008642
phamtung-23 <ptung230801@gmail.com>
2022-01-14 20:11:08 +0700

    sửa file profieleTP

--- manager/profileTP.php
+++ manager/profileTP.php
@@ -271,7 +271,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<!-- Sử dụng link tuyệt đối tính từ root, vì vậy có dấu / đầu tiên -->
-	<script src="/main.js"></script> 
+	<script src="../main.js"></script> 
  
 
 </body>
