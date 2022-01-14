b22977b1a4e8a29d4ab692802b6c3cbcdb28166b
DeKuTaRo <thanhtuan09102001@gnail.com>
2022-01-14 16:37:43 +0700

    update info profileNV.php

--- profileNV.php
+++ profileNV.php
@@ -40,6 +40,33 @@
     // print_r($admin_account['gioiTinh'] == 1);
     $avatar_path = "./images/" . $employee_account['anhDaiDien'];
 
+
+    if (isset($_POST['submit'])) {
+        $message = '';
+        if (!isset($_POST['employee-birthday']) || !isset($_POST['employee-address']) || !isset($_POST['employee-sex']) ||
+        !isset($_POST['employee-number']) || !isset($_POST['employee-email'])) {
+            $message = "Vui lòng nhập đầy đủ thông tin";
+            die($message);
+        } else {
+
+            $employee_id = $_SESSION['maNhanVien'];
+            $employee_birthday = $_POST['employee-birthday'];
+            $employee_address = $_POST['employee-address'];
+            $employee_sex = $_POST['employee-sex'];
+            $employee_number = $_POST['employee-number'];
+            $employee_email = $_POST['employee-email'];
+
+            $sql = "UPDATE NhanVien SET ngaySinh = ?, gioiTinh = ?, diaChi = ?, sdt = ?, email = ? WHERE maNhanVien = '$employee_id' ";
+            $conn = connect_db();
+            $stm = $conn->prepare($sql);
+            $stm -> bind_param("sisss", $employee_birthday, $employee_sex, $employee_address, $employee_number, $employee_email);
+            $stm -> execute();
+
+            if ($stm->affected_rows == 1) {
+                header("Location: profileNV.php");
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -113,63 +140,67 @@
         <div class="bg-light card pr-5">
             <div class="row py-4">
                 <div class="col-6 py-4 d-flex flex-column justify-content-between">
-                    <div class="form-group row align-items-center">
-                        <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-id">Mã nhân viên</label>
-                        <div class="col-sm-8">
-                            <input disabled id="employee-id" value="<?= $_SESSION['maNhanVien'] ?>" type="text" class="form-control">
+                    <form action="" method="POST">
+
+                        <div class="form-group row align-items-center">
+                            <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-id">Mã nhân viên</label>
+                            <div class="col-sm-8">
+                                <input disabled id="employee-id" value="<?= $_SESSION['maNhanVien'] ?>" type="text" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row align-items-center">
-                        <label class="control-label text-right col-sm-4 p-0 m-0" for="department-id">Mã phòng ban</label>
-                        <div class="col-sm-8">
-                            <input disabled id="department-id" value="<?= $maPhongBan ?>" type="text" class="form-control">
+                        <div class="form-group row align-items-center">
+                            <label class="control-label text-right col-sm-4 p-0 m-0" for="department-id">Mã phòng ban</label>
+                            <div class="col-sm-8">
+                                <input disabled id="department-id" value="<?= $maPhongBan ?>" type="text" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row align-items-center">
-                        <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-birthday">Ngày sinh</label>
-                        <div class="col-sm-8">
-                            <input disabled id="employee-birthday" value="<?= $birthDay ?>" type="date" class="form-control">
+                        <div class="form-group row align-items-center">
+                            <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-birthday">Ngày sinh</label>
+                            <div class="col-sm-8">
+                                <input disabled id="employee-birthday" name="employee-birthday" value="<?= $birthDay ?>" type="date" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row align-items-center">
-                        <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-address">Địa chỉ</label>
-                        <div class="col-sm-8">
-                            <input disabled id="employee-address" value="<?= $diaChi ?>" type="text" class="form-control">
+                        <div class="form-group row align-items-center">
+                            <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-address">Địa chỉ</label>
+                            <div class="col-sm-8">
+                                <input disabled id="employee-address" name="employee-address" value="<?= $diaChi ?>" type="text" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row align-items-center">
-                    <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-sex">Giới tính:</label>
-                        <div class="col-sm-8 text-left">
-                        <?php
-                            if ($gioiTinh === 1) {
+                        <div class="form-group row align-items-center">
+                        <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-sex">Giới tính:</label>
+                            <div class="col-sm-8 text-left">
+                            <?php
+                                if (intval($gioiTinh) === 1 ) {
+                                    ?>
+                                    
+                                        <label class="radio-inline mb-0 mr-3"><input disabled id="male" value="1" class="mr-2" type="radio" name="employee-sex" checked>Nam</label>
+                                        <label class="radio-inline mb-0"><input disabled id="female" value="0" class="mr-2" type="radio" name="employee-sex">Nữ</label>
+                                    <?php
+                                } else {
+                                    ?>
+                                        <label class="radio-inline mb-0 mr-3"><input disabled id="male" value="1" class="mr-2" type="radio" name="employee-sex">Nam</label>
+                                        <label class="radio-inline mb-0"><input disabled id="female" value="0" class="mr-2" type="radio" name="employee-sex" checked>Nữ</label>
+                                    <?php
+                                    }
                                 ?>
-                                    <label class="radio-inline mb-0 mr-3"><input disabled id="male" class="mr-2" type="radio" name="employee-sex" checked>Nam</label>
-                                    <label class="radio-inline mb-0"><input disabled id="female" class="mr-2" type="radio" name="employee-sex">Nữ</label>
-                                <?php
-                            } else {
-                                ?>
-                                    <label class="radio-inline mb-0 mr-3"><input disabled id="male" class="mr-2" type="radio" name="employee-sex">Nam</label>
-                                    <label class="radio-inline mb-0"><input disabled id="female" class="mr-2" type="radio" name="employee-sex" checked>Nữ</label>
-                                <?php
-                                }
-                            ?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row align-items-center">
-                        <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-number">Số điện thoại</label>
-                        <div class="col-sm-8">
-                            <input disabled id="employee-number" value="<?= $soDT ?>" type="number" class="form-control">
+                        <div class="form-group row align-items-center">
+                            <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-number">Số điện thoại</label>
+                            <div class="col-sm-8">
+                                <input disabled id="employee-number" name="employee-number" value="<?= $soDT ?>" type="number" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row align-items-center">
-                        <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-email">Email</label>
-                        <div class="col-sm-8">
-                            <input disabled id="employee-email" value="<?= $email ?>" type="email" class="form-control">
+                        <div class="form-group row align-items-center">
+                            <label class="control-label text-right col-sm-4 p-0 m-0" for="employee-email">Email</label>
+                            <div class="col-sm-8">
+                                <input disabled id="employee-email" name="employee-email" value="<?= $email ?>" type="email" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group text-center">
-                        <button data-toggle="modal" data-target="#confirm-edit-admin-profile-modal" disabled id="btn-confirm-edit-employee-profile" class="btn btn-primary">Xác nhận</button>
-                    </div>
+                        <div class="form-group text-center">
+                            <button type="submit" name="submit" disabled id="btn-confirm-edit-employee-profile" class="btn btn-primary">Xác nhận</button>
+                        </div>
+                    </form>
                 </div>
                 <div class="col-6 d-flex border-none border rounded">              
                     <div class="card-employee w-100 m-auto">
@@ -201,18 +232,40 @@
                             <label class="custom-file-label" for="employee-avatar">Choose file</label>
                         </div>
                         <div class="form-group mt-3">
-                            <div id="change-employee-avatar-error-message" class="text-center alert-danger font-weight-bold"></div>
+                            <?php
+                                require_once('employee/handle_change_employee_avatar.php');
+                            ?>
+                            <div id="change-employee-avatar-error-message" class="text-center alert-danger font-weight-bold"><?= implode($errors)  ?></div>
                         </div>
                     </div>
                     <div  class="modal-footer">
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Thoát</button>
-                        <button onclick="handleChangeAdminAvatar(event);" type="submit" class="btn btn-primary">Thay đổi</button>
+                        <button type="submit" class="btn btn-primary">Thay đổi</button>
                     </div>   
                 </form>      
             </div>
         </div>
     </div>
 
+
+     <!-- Dialog xác nhận cập nhật thông tin
+     <div id="confirm-edit-employee-profile-modal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                    <h4 class="modal-title">Cập nhật thông tin</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <p>Bạn có chắc rằng muốn cập nhật thông tin nhân viên <strong><?= $_SESSION['hoTen'] ?></strong> không?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Hủy bỏ</button>
+                    <button type="submit" name="submit-info" class="btn btn-danger">Đồng ý</button>
+                </div>
+            </div>
+        </div>
+    </div> -->
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -223,16 +276,19 @@
     <script>
         let employee_birthday = document.getElementById('employee-birthday');
         let employee_address = document.getElementById('employee-address');
+        let employee_sex_male = document.getElementById('male');
+        let employee_sex_female = document.getElementById('female');
         let employee_sex = document.getElementById('employee-sex');
         let employee_number = document.getElementById('employee-number');
         let employee_email = document.getElementById('employee-email');
-        let btn_confirm_edit_employee_profile = document.getElementById('btn-confirm-edit-employee');
+        let btn_confirm_edit_employee_profile = document.getElementById('btn-confirm-edit-employee-profile');
 
 
         function enableEditEmployeeProfileMode() {
             employee_birthday.disabled = false;
             employee_address.disabled = false;
-            employee_sex.disabled = false;
+            employee_sex_male.disabled = false;
+            employee_sex_female.disabled = false;
             employee_number.disabled = false;
             employee_email.disabled = false;
             btn_confirm_edit_employee_profile.disabled = false;
